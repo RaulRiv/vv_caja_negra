fcb94d2cd66ef3a72846a6135d8bdf3dc40dfe19
MarioRojoVicente <mario.rojo.vicente@alumnos.upm.es>
2021-11-21 21:46:13 +0100

    Revert "Revert "Size tests and scheme for other tests""
    This reverts commit 9140a2d3f97fa1a227931ab63279a2a0ca799943.

---
+++ src/test/java/com/practica/cajanegra/IndexOfTest.java
@@ -0,0 +1,18 @@
+package com.practica.cajanegra;
+
+public class IndexOfTest {
+
+    //Elemento está en primera posición
+
+    //Elemento está en el medio
+
+    //Elemento está en última posición
+
+    //No está el elemento
+
+    //La lista es null
+
+    //El elemento no es válido
+
+
+}
---
+++ src/test/java/com/practica/cajanegra/ReverseTest.java
@@ -0,0 +1,13 @@
+package com.practica.cajanegra;
+
+public class ReverseTest {
+
+
+    //Lista vacía
+
+    //Lista null
+
+    //Lista de un elemento
+
+    //lista de n elementos
+}
